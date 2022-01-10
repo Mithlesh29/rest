81cb73f7d95f155f21285bf3a2a268f8e12f775e
Mithlesh29 <mithlesh29997@gmail.com>
2022-01-10 11:23:18 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const expree = require("express")
+const app = express()
+const port=4000;
+app.use = (express.json)
+app.get("/",print)
+app.listen(port, () => {console.log("server is connected")
+} )
